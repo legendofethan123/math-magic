86e23f275053d2571c7e74141f497ff838d9460f
Ethan Gonzalez <104691236+legendofethan123@users.noreply.github.com>
2022-05-01 11:18:55 -0500

    Create magic.java

---
+++ magic.java
@@ -0,0 +1,16 @@
+public class Magic {
+	public static void main(String[] args) {
+    // the original number set 
+int myNumber = 18;
+int stepOne = myNumber * myNumber;
+int stepTwo = stepOne + myNumber;
+int stepThree = stepTwo / myNumber;
+int stepFour = stepThree + 17;
+int stepFive = stepFour - myNumber;
+int stepSix = stepFive / 6;
+int lastStep = stepSix;
+System.out.println(lastStep);
+		
+
+	}
+}
